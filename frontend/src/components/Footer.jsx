492be57a27771c4ae7af475f23785a0bb382b8d8
priyanshu pundir <priyanshupundir345@gmail.com>
2025-07-14 13:00:35 +0530

    changes in footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className='flex items-center justify-between gap-4 py-3 mt-20'>
       
       <img src={assets.logo}  width={150}/>
-      <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @Greatstack |All right reserved</p>
+      <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright|All right reserved</p>
 
       <div className='flex gap-2.5'>
         <img src={assets.facebook_icon} alt="" width={35} />
